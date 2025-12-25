ec539ecbd4db2f8542f6d84639cfd493ff9af9a6
Airat <airatchigashov@gmail.com>
2025-12-25 15:07:49 +0300

    create loginUser and logoutUser

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import { query } from "../config/db.js";
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken';
 
 // ! Регистрация пользователя
 export const registerUser = async (req, res) => {
@@ -57,7 +58,7 @@ export const registerUser = async (req, res) => {
    INSERT INTO Users (role_id, name, email, password_hash) VALUES ($1, $2, $3, $4) RETURNING *
    `;
       const addUser = await query(sqlAddUser, [roleId, name, email, hashPassword]);
-      const {password_hash, ...data} = addUser.rows[0];
+      const { password_hash, ...data } = addUser.rows[0];
 
       return res.status(201).json({
          message: 'Успешная регистрация',
@@ -75,3 +76,92 @@ export const registerUser = async (req, res) => {
    };
 
 };
+
+// ! Вход пользователя
+export const loginUser = async (req, res) => {
+   const { email, password } = req.body;
+
+   if (!email || !password) {
+      return res.status(400).json({
+         message: 'Заполните все поля ввода',
+         error: true,
+         success: false
+      })
+   };
+
+   try {
+      // Находим пользователя по email
+      const findUserByEmail = await query(`SELECT * FROM Users WHERE email = $1`, [email]);
+
+      // Если пользователь не найден то ошибка
+      if (findUserByEmail.rows.length === 0) {
+         return res.status(404).json({
+            message: "Неверный email или пароль",
+            error: true,
+            success: false
+         })
+      };
+
+      // Если пользователь найден то сравниваем пароль и создаем токен
+      const comparePassword = await bcrypt.compare(password, findUserByEmail.rows[0].password_hash);
+
+      if (!comparePassword) {
+         return res.status(400).json({
+            message: "Неверный email или пароль",
+            error: true,
+            success: false
+         })
+      };
+
+      const token = await jwt.sign(
+         { userId: findUserByEmail.rows[0].user_id, roleId: findUserByEmail.rows[0].role_id },
+         process.env.JWT_SECRET,
+         { expiresIn: '7d' }
+      );
+
+      res.cookie("token", token, {
+         httpOnly: true, // Запрещает доступ к куки через клиентский JavaScript
+         secure: true, // обязательно для https
+         sameSite: "none", // разрешает кросс-доменные куки
+      });
+
+      const { password_hash, ...data } = findUserByEmail.rows[0]
+
+      return res.status(200).json({
+         message: "Вход выполнен",
+         error: false,
+         success: true,
+         user: data
+      })
+   } catch (error) {
+      return res.status(500).json({
+         message: 'Ошибка при входе на сервере',
+         error: true,
+         success: false
+      });
+   }
+};
+
+// ! Выход
+export const logoutUser = async (req, res) => {
+   try {
+      res.clearCookie("token", {
+         httpOnly: true,
+         secure: true,
+         sameSite: "none",
+      });
+
+      return res.status(200).json({
+         message: "Выход из системы успешен",
+         success: true,
+         error: false,
+         user: null,
+      });
+   } catch (error) {
+      return res.status(500).json({
+         message: 'Ошибка при выходе на сервере',
+         error: true,
+         success: false
+      });
+   }
+}
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,10 +1,16 @@
 import express from 'express';
-import { registerUser } from '../controllers/auth.controller.js';
+import { loginUser, logoutUser, registerUser } from '../controllers/auth.controller.js';
 
 const authRouter = express.Router();
 
 // Регистрация пользователя
 authRouter.post('/register', registerUser);
+
+// Вход пользователя
+authRouter.post('/login', loginUser);
+
+// Выход пользователя
+authRouter.get('/logout', logoutUser);
 
 
 export default authRouter;
