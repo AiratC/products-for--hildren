e59284ca5311847260147c82fe29e4fae11b0838
Airat <airatchigashov@gmail.com>
2026-01-13 15:49:35 +0300

    write addReviews in router

--- backend/routes/reviews.route.js
+++ backend/routes/reviews.route.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import { addReviews } from '../controllers/reviews.controller.js';
 
 const reviewsRouter = express.Router();
+
+reviewsRouter.post('/add-reviews', addReviews);
 
 
 export default reviewsRouter;
