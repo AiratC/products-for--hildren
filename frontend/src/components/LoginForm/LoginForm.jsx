31e863585faad8b25bbbdf842c781907689b16d4
Airat <airatchigashov@gmail.com>
2026-02-10 10:49:04 +0300

    create file store.js, create state formData and function handleChange

--- frontend/src/components/LoginForm/LoginForm.jsx
+++ frontend/src/components/LoginForm/LoginForm.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './LoginForm.module.css';
 import { Link, useNavigate } from 'react-router-dom';
 
 
 const LoginForm = ({ onClose }) => {
+   const [formData, setFormData] = useState({
+      email: '',
+      password: ''
+   });
+
    const navigate = useNavigate();
 
    const handleClickRegister = () => {
@@ -11,14 +16,22 @@ const LoginForm = ({ onClose }) => {
       onClose();
    }
 
+   const handleChange = (event) => {
+      const { name, value } = event.target;
+      setFormData(prevData => ({
+         ...prevData,
+         [name]: value
+      }))
+   };
+
    return (
       <div className={styles.login}>
          <div onClick={handleClickRegister}>
             <h3 className={styles.title}>Регистрация</h3>
          </div>
          <div className={styles.form}>
-            <input type="email" placeholder="Электронный адрес" className={styles.input} />
-            <input type="password" placeholder="Пароль" className={styles.input} />
+            <input onChange={handleChange} value={formData.email} name='email' type="email" placeholder="Электронный адрес" className={styles.input} />
+            <input onChange={handleChange} value={formData.password} name='password' type="password" placeholder="Пароль" className={styles.input} />
             <div className={styles.footer}>
                <button className={styles.loginBtn}>Войти</button>
                <button className={styles.forgotBtn}>Забыли пароль?</button>
