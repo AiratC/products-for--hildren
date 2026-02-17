7c1bd4770438e50651167202bae82799a4b02320
Airat <airatchigashov@gmail.com>
2026-02-17 17:49:54 +0300

    delete useMemo

--- frontend/src/pages/OrdersPage/OrdersPage.jsx
+++ frontend/src/pages/OrdersPage/OrdersPage.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo } from 'react';
+import React, { useCallback } from 'react';
 import styles from './OrdersPage.module.css';
 import { useNavigate } from 'react-router';
 
