c07f0770f1faeb8f193a9f9308f0896ac810d1d4
Airat <airatchigashov@gmail.com>
2026-02-18 17:01:31 +0300

    write route in order.route.js

--- backend/routes/order.route.js
+++ backend/routes/order.route.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import { protect } from '../middleware/authMiddleware.js';
-import { createOrder } from '../controllers/order.controller.js';
+import { createOrder, getMyOrders } from '../controllers/order.controller.js';
 
 const orderRouter = express.Router();
 
 // Создание заказа
 orderRouter.post('/create-order', protect, createOrder);
+
+// Получаем все заказы пользователя
+orderRouter.get('/get-orders', protect, getMyOrders);
 
 
 export default orderRouter;
